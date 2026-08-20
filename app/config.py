0e1c34ad8b2937b401caad00e1a97553bd67f36d
jothi6506 <magarajothik523@gmail.com>
2026-08-20 22:45:17 +0530

    Optimize database connection pool options for speed

--- app/config.py
+++ app/config.py
@@ -12,7 +12,9 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
         "pool_pre_ping": True,
-        "pool_recycle": 300,
+        "pool_recycle": 1800,
+        "pool_size": 10,
+        "max_overflow": 20,
     }
 
     # Uploads
